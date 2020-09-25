a2d3fac96e0798a6fabaab93f87053eda3cd5cb4
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-09-25 09:50:36 +0530

    Methods in repository added

--- CustomerMicroservice/Repository/CustomerRep.cs
+++ CustomerMicroservice/Repository/CustomerRep.cs
@@ -1,4 +1,5 @@
-﻿using CustomerMicroservice.Models;
+using CustomerMicroservice.Models;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -11,13 +12,37 @@ namespace CustomerMicroservice.Repository
 {
     public class CustomerRep : ICustomerRep
     {
-        public List<Customer> getCustomerList()
+        public List<Customer> customers = new List<Customer>
         {
-            List<Customer> customerList = new List<Customer>
+            new Customer{CustId=1, Name="SB",Address="Dumdum",DOB="05-09-1997",PanNo="CGLBP002"}
+        };
+        //int CustId = 1;
+        Uri baseAddress = new Uri("https://localhost:44372/api/Account");
+        HttpClient client;
+        public CustomerRep()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+        public CustomerCreationStatus createCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            string data = JsonConvert.SerializeObject(customer.CustId);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(client.BaseAddress + "/createAccount/", content).Result;
+            if (response.IsSuccessStatusCode)
             {
-                new Customer{id=1,Name="S B",DOB="05/09/1997",Address="ABC",PanNo="CGLBP1000",Email="sb@gmail.com",Password="123456"}
-            };
-            return customerList;
+                var ob = new CustomerCreationStatus();
+                ob.CustomerId = customer.CustId;
+                ob.Message = "Success. Current and Savings account also created";
+                return ob;
+            }
+            return null;
+        }
+
+        public Customer getCustomerDetails(int CustId)
+        {
+            return customers.Where(c => c.CustId == CustId).FirstOrDefault();
         }
     }
 }
