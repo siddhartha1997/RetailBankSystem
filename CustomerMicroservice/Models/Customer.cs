560a35630f6ece9f0dd9db04a026bc12e588b53f
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-09-25 09:47:32 +0530

    Updated Model Class

--- CustomerMicroservice/Models/Customer.cs
+++ CustomerMicroservice/Models/Customer.cs
@@ -8,14 +8,11 @@ namespace CustomerMicroservice.Models
 {
     public class Customer
     {
-        [Key]
-        public int id { get; set; }
+        public int CustId { get; set; }
         public string Name { get; set; }
-        public string DOB { get; set; }
         public string Address { get; set; }
+        public string DOB { get; set; }
         public string PanNo { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
 
     }
 }
